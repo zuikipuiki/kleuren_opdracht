b80cb5135804efc2b55f52b8f679f6746c3eb4ee
zuikipuiki <31111953+zuikipuiki@users.noreply.github.com>
2021-10-29 12:46:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let hamburger = document.querySelector(".hamburger-menu");
+let navList = document.querySelector("ul");
+
+let navHome = document.querySelector(".home a");
+let navYellow = document.querySelector(".yellow a");
+let navOrange = document.querySelector(".orange a");
+let navRed = document.querySelector(".red a");
+let navGreen = document.querySelector(".green a");
+
+
+
+hamburger.addEventListener("click", function() {
+ navList.style.visibility = "visible";
+});
+
+
+
+//CLICK OM KLEUREN TE VERANDEREN
+// HOME 
+navHome.addEventListener("click", function () {
+ document.body.style.backgroundColor = "rgb(201, 196, 196)";
+ });
+// YELLOW 
+navYellow.addEventListener("click", function () {
+ document.body.style.backgroundColor = "rgb(243, 233, 87)";
+});
+// ORANGE
+navOrange.addEventListener("click", function () {
+ document.body.style.backgroundColor = "rgb(252, 155, 76)";
+ });
+// RED 
+navRed.addEventListener("click", function () {
+ document.body.style.backgroundColor = "rgb(241, 72, 72)";
+ });
+// GREEN
+navGreen.addEventListener("click", function () {
+ document.body.style.backgroundColor = "rgb(109, 231, 98)";
+ });
+
